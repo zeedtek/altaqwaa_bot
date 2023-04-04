c9e7922595e100801d78ada1cf143c08ce4186a9
rn0x <76129163+rn0x@users.noreply.github.com>
2023-04-05 02:04:38 +0300

    Update error_handling.js

--- module/error_handling.js
+++ module/error_handling.js
@@ -36,7 +36,7 @@ export default async (error, client) => {
 
                 let fileJson = fs?.readJsonSync(path?.join(__dirname, `./database/${id_user}.json`));
                 fileJson.evenPost = false;
-                fs.writeJsonSync(path.join(__dirname, `./database/${id_user}.json`), fileJson);     
+                fs.writeJsonSync(path.join(__dirname, `./database/${id_new}.json`), fileJson);     
             }
 
         }
@@ -53,6 +53,12 @@ export default async (error, client) => {
             let id_user = error.on.payload.chat_id
             let message = 'طلب غير صالح: لا توجد حقوق كافية لإرسال الصور إلى الدردشة'
             await client.telegram.sendMessage(id_user, message);
+        }
+
+        else if (error.response.description === "Bad Request: not enough rights to send text messages to the chat") {
+
+            let id_user = error.on.payload.chat_id
+            await client.telegram.sendMessage(id_user, error?.on?.payload?.text);
         }
 
         else if (error.response.description === "Forbidden: bot was blocked by the user") {
